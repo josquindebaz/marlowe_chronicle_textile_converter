288cee676a47fc0d5abc4dcf3abbcf325d49212b
Josquin Debaz <josquindebaz@protonmail.com>
2025-01-10 23:34:15 +0100

    Extract to textile_utils

---
+++ tests/test_textile_utils.py
@@ -0,0 +1,147 @@
+import textile_utils
+
+
+def test_format_links():
+    content = 'any link https://any.link/something can do'
+    expected = 'any link "https://any.link/something":https://any.link/something can do'
+
+    result = textile_utils.format_links(content)
+
+    assert result == expected
+
+
+def test_format_links_does_not_change_when_no_link():
+    content = "no link"
+
+    result = textile_utils.format_links(content)
+
+    assert result == content
+
+
+def test_format_links_change_gspr_free():
+    content = "Voici un lien http://gspr.ehess.free.fr/ à consulter."
+    expected = 'Voici un lien "http://gspr-ehess.com/":http://gspr-ehess.com/ à consulter.'
+
+    result = textile_utils.format_links(content)
+
+    assert result == expected
+
+
+def test_format_links_change_prosperologie_ip():
+    content = "Voici un lien http://92.243.27.161 à consulter."
+    expected = 'Voici un lien "http://prosperologie.org":http://prosperologie.org à consulter.'
+
+    result = textile_utils.format_links(content)
+
+    assert result == expected
+
+
+def test_format_links_change_multiple_links():
+    content = "Voici deux liens http://92.243.27.161 et http://gspr.ehess.free.fr/ à consulter."
+    expected = ('Voici deux liens "http://prosperologie.org":http://prosperologie.org et '
+                '"http://gspr-ehess.com/":http://gspr-ehess.com/ à consulter.')
+
+    result = textile_utils.format_links(content)
+
+    assert result == expected
+
+
+def test_format_links_change_images():
+    content = "This is an https://www.poetsgraves.co.uk/images/borgesgrave.jpg image"
+    expected = 'This is an !https://www.poetsgraves.co.uk/images/borgesgrave.jpg! image'
+
+    result = textile_utils.format_links(content)
+
+    assert result == expected
+
+
+def test_format_links_change_youtube():
+    content = "This is a https://www.youtube.com/watch?v=FK-KC2aQpcI video"
+    expected = ("This is a\n\n<iframe frameborder='0'  width='500'  height='352' "
+                "src='http://www.youtube.com/embed/FK-KC2aQpcI'  "
+                "allowfullscreen='allowfullscreen'></iframe>\n\n video")
+
+    result = textile_utils.format_links(content)
+
+    assert result == expected
+
+
+def test_format_links_change_youtu_be():
+    content = "This is a https://youtu.be/watch?v=FK-KC2aQpcI video"
+    expected = ("This is a\n\n<iframe frameborder='0' width='500' height='352' "
+                "src='http://www.youtube.com/embed/watch?v=FK-KC2aQpcI'  "
+                "allowfullscreen='allowfullscreen'></iframe>\n\n video")
+
+    result = textile_utils.format_links(content)
+
+    assert result == expected
+
+
+def test_format_links_change_pdf():
+    content = "This is a https://gspr-ehess.com/documents/ObsEnv2003.pdf pdf"
+    expected = ('This is '
+                'a"https://gspr-ehess.com/documents/ObsEnv2003.pdf":https://gspr-ehess.com/documents/ObsEnv2003.pdf\n'
+                '\n'
+                '<object '
+                'data="https://gspr-ehess.com/documents/ObsEnv2003.pdf#toolbar=0&navpanes=0&view=Fit" '
+                'width="500" height="650" type="application/pdf"></object> pdf')
+
+    result = textile_utils.format_links(content)
+
+    assert result == expected
+
+
+def test_format_links_change_daily_embed():
+    content = "This is a http://www.dailymotion.com/embed/video/xiwwfd video"
+    expected = ('This is a\n'
+                '\n'
+                '<iframe frameborder="0" width="500" height="352" '
+                'src="http://www.dailymotion.com/embed/video/xiwwfd"></iframe>\n'
+                '\n'
+                ' video')
+    result = textile_utils.format_links(content)
+
+    assert result == expected
+
+
+def test_format_links_change_daily_video():
+    content = "This is a http://www.dailymotion.com/video/x387ig4 video"
+    expected = ('This is a\n'
+                '\n'
+                '<iframe frameborder="0" width="500" height="352" '
+                'src="http://www.dailymotion.com/embed/video/x387ig4"></iframe>\n'
+                '\n'
+                ' video')
+
+    result = textile_utils.format_links(content)
+
+    assert result == expected
+
+
+def test_format_links_change_vimeo():
+    content = "This is a http://vimeo.com/7021433 video"
+    expected = ('This is a\n'
+                '\n'
+                '<iframe frameborder="0" width="500" height="352" '
+                'src="https://player.vimeo.com/video/7021433" webkitAllowFullScreen '
+                'mozallowfullscreen allowFullScreen></iframe>\n'
+                '\n'
+                ' video')
+
+    result = textile_utils.format_links(content)
+
+    assert result == expected
+
+
+def test_format_links_change_canal_u():
+    content = "This is a http://www.canal-u.tv/video/iap/exoplanetes_la_surprenante_diversite_des_autres_mondes_du_cosmos.18397 video"
+    expected = ('This is a\n'
+                '\n'
+                '<iframe '
+                'src="https://www.canal-u.tv/video/iap/embed.1/exoplanetes_la_surprenante_diversite_des_autres_mondes_du_cosmos.18397" '
+                'width="550" height="306" frameborder="0" allowfullscreen '
+                'scrolling="no"></iframe> video')
+
+    result = textile_utils.format_links(content)
+
+    assert result == expected
---
+++ textile_utils.py
@@ -0,0 +1,84 @@
+import re
+
+
+def format_links(block):
+    """format link for textile according to their target type
+        including video and images"""
+
+    block = re.sub("gspr.ehess.free.fr", "gspr-ehess.com", block)
+    block = re.sub("92.243.27.161", "prosperologie.org", block)
+
+    motif_images = re.compile(r"(https?\S*.(jpg|png|gif|jpeg|JPG|img))")
+
+    fragments = re.split(r'[^:](http\S*)', block)
+    block = fragments[0]
+
+    for i, fragment in enumerate(fragments[1:]):
+        if i % 2 == 0:
+            if motif_images.search(fragment):
+                fragment = re.sub(r"(https?\S*.(jpg|png|gif|jpeg|JPG|img))",
+                                  " !\\1!", fragment)
+            elif re.search("youtube.com/watch", fragment):
+                fragment = re.sub(r'https?://www.youtube.com/watch?'
+                                  r'\S*v=([^\s&]*)',
+                                  "\n\n<iframe frameborder='0'  width='500' "
+                                  " height='352' "
+                                  "src='http://www.youtube.com/embed/\\1' "
+                                  " allowfullscreen='allowfullscreen'>"
+                                  "</iframe>\n\n",
+                                  fragment)
+            elif re.search("youtu.be", fragment):
+                fragment = re.sub(r"https?://youtu.be/(.*)",
+                                  "\n\n<iframe frameborder='0' width='500' "
+                                  "height='352' "
+                                  "src='http://www.youtube.com/embed/\\1'  "
+                                  "allowfullscreen='allowfullscreen'>"
+                                  "</iframe>\n\n",
+                                  fragment)
+            elif re.search(r"https?://\S*\.pdf", fragment):
+                fragment = re.sub(r"(https?://\S*\.pdf)",
+                                  '"\\1":\\1\n\n<object '
+                                  'data="\\1#toolbar=0&navpanes=0&view=Fit" '
+                                  'width="500" height="650" '
+                                  'type="application/pdf"></object>',
+                                  fragment)
+            elif re.search(r"https?://www.dailymotion.com/\S*", fragment):
+                if re.search(r"https?://www.dailymotion.com/embed/video/\S*",
+                             fragment):
+                    fragment = re.sub(r"(https?://www.dailymotion.com/"
+                                      r"embed/video/\S*)",
+                                      '\n\n<iframe frameborder="0" '
+                                      'width="500" height="352" '
+                                      'src="\\1"></iframe>\n\n',
+                                      fragment)
+                elif re.search(r"https?://www.dailymotion.com/video/.*",
+                               fragment):
+                    fragment = re.sub(r"(https?://www.dailymotion.com/)"
+                                      r"(video/[^_]*).*\s*",
+                                      '\n\n<iframe frameborder="0" '
+                                      'width="500" height="352" '
+                                      'src="\\1embed/\\2\"></iframe>\n\n',
+                                      fragment)
+            elif re.search(r"https?://vimeo.com/\d+", fragment):
+                fragment = re.sub(r"https?://vimeo.com/(\d+)",
+                                  '\n\n<iframe frameborder="0" '
+                                  'width="500" height="352" '
+                                  'src="https://player.vimeo.com/video/\\1\" '
+                                  'webkitAllowFullScreen mozallowfullscreen '
+                                  'allowFullScreen></iframe>\n\n',
+                                  fragment)
+            elif re.search(r"https?://www.canal-u.tv/video/S*", fragment):
+                fragment = re.sub(r"https?://www.canal-u.tv/video/(\S*)"
+                                  r"/(\S*)",
+                                  '\n\n<iframe src="https://www.canal-u.tv/'
+                                  'video/\\1/embed.1/\\2" width="550" '
+                                  'height="306" frameborder="0" '
+                                  'allowfullscreen scrolling="no"></iframe>',
+                                  fragment)
+            else:
+                fragment = re.sub(r'(https?://\S*)',
+                                  ' "\\1":\\1',
+                                  fragment)
+        block += fragment
+
+    return block
