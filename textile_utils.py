bdb89a2e201bf01ffdbe4dbce97ecc246414902b
Josquin Debaz <josquindebaz@protonmail.com>
2025-01-11 13:07:46 +0100

    Use dictionary for substitutions

--- textile_utils.py
+++ textile_utils.py
@@ -5,14 +5,24 @@ def format_links(block):
     """format link for textile according to their target type
         including video and images"""
 
-    image_pattern = re.compile(r"(https?\S*.(jpg|png|gif|jpeg|JPG|img))")
-    youtube_pattern = re.compile(r'https?://www.youtube.com/watch?\S*v=([^\s&]*)')
-    youtu_be_pattern = re.compile(r"https?://youtu.be/(.*)")
-    pdf_pattern = re.compile(r"(https?://\S*\.pdf)")
-    daily_embed_pattern = re.compile(r"(https?://www.dailymotion.com/embed/video/\S*)")
-    daily_video_pattern = re.compile(r"(https?://www.dailymotion.com/)(video/[^_]*).*\s*")
-    vimeo_pattern = re.compile(r"https?://vimeo.com/(\d+)")
-    canal_u_pattern = re.compile(r"https?://www.canal-u.tv/video/(\S*)/(\S*)")
+    substitutions = {
+        r"(https?\S*.(jpg|png|gif|jpeg|JPG|img))":
+            " !\\1!",
+        r'https?://www.youtube.com/watch?\S*v=([^\s&]*)':
+            "\n\n<iframe frameborder='0' width='500' height='352' src='http://www.youtube.com/embed/\\1' allowfullscreen='allowfullscreen'></iframe>\n\n",
+        r"https?://youtu.be/(.*)":
+            "\n\n<iframe frameborder='0' width='500' height='352' src='http://www.youtube.com/embed/\\1' allowfullscreen='allowfullscreen'></iframe>\n\n",
+        r"(https?://\S*\.pdf)":
+            '"\\1":\\1\n\n<object data="\\1#toolbar=0&navpanes=0&view=Fit" width="500" height="650" type="application/pdf"></object>',
+        r"(https?://www.dailymotion.com/embed/video/\S*)":
+            '\n\n<iframe frameborder="0" width="500" height="352" src="\\1"></iframe>\n\n',
+        r"(https?://www.dailymotion.com/)(video/[^_]*).*\s*":
+            '\n\n<iframe frameborder="0" width="500" height="352" src="\\1embed/\\2\"></iframe>\n\n',
+        r"https?://vimeo.com/(\d+)":
+            '\n\n<iframe frameborder="0" width="500" height="352" src="https://player.vimeo.com/video/\\1\" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>\n\n',
+        r"https?://www.canal-u.tv/video/(\S*)/(\S*)":
+            '\n\n<iframe src="https://www.canal-u.tv/video/\\1/embed.1/\\2" width="550" height="306" frameborder="0" allowfullscreen scrolling="no"></iframe>'
+    }
 
     fragments = re.split(r'[^:](http\S*)', block)
     block = fragments[0]
@@ -21,53 +31,11 @@ def format_links(block):
         if i % 2 != 0:
             pass
 
-        if image_pattern.search(fragment):
-            fragment = image_pattern.sub(" !\\1!", fragment)
-
-        elif youtube_pattern.search(fragment):
-            replacement = "\n\n<iframe frameborder='0' width='500' height='352' " \
-                          "src='http://www.youtube.com/embed/\\1' allowfullscreen='allowfullscreen'></iframe>\n\n"
-
-            fragment = youtube_pattern.sub(replacement, fragment)
-
-        elif youtu_be_pattern.search(fragment):
-            replacement = "\n\n<iframe frameborder='0' width='500' height='352' " \
-                          "src='http://www.youtube.com/embed/\\1' allowfullscreen='allowfullscreen'></iframe>\n\n"
-
-            fragment = youtu_be_pattern.sub(replacement, fragment)
-
-        elif pdf_pattern.search(fragment):
-            replacement = '"\\1":\\1\n\n<object data="\\1#toolbar=0&navpanes=0&view=Fit" ' \
-                          'width="500" height="650" type="application/pdf"></object>'
-
-            fragment = pdf_pattern.sub(replacement, fragment)
-
-        elif daily_embed_pattern.search(fragment):
-            replacement = '\n\n<iframe frameborder="0" width="500" height="352" src="\\1"></iframe>\n\n'
-
-            fragment = daily_embed_pattern.sub(replacement, fragment)
-
-        elif daily_video_pattern.search(fragment):
-            replacement = '\n\n<iframe frameborder="0" width="500" height="352" src="\\1embed/\\2\"></iframe>\n\n'
-
-            fragment = daily_video_pattern.sub(replacement, fragment)
-
-        elif vimeo_pattern.search(fragment):
-            replacement = '\n\n<iframe frameborder="0" width="500" height="352" src="https://player.vimeo.com/video/\\1\" ' \
-                          'webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>\n\n'
-
-            fragment = vimeo_pattern.sub(replacement, fragment)
-
-        elif canal_u_pattern.search(fragment):
-            replacement = '\n\n<iframe src="https://www.canal-u.tv/video/\\1/embed.1/\\2" width="550" ' \
-                          'height="306" frameborder="0" allowfullscreen scrolling="no"></iframe>'
-
-            fragment = canal_u_pattern.sub(replacement, fragment)
-
+        if not re.search("|".join(substitutions.keys()), fragment):
+            fragment = re.sub(r'(https?://\S*)', ' "\\1":\\1', fragment)
         else:
-            fragment = re.sub(r'(https?://\S*)',
-                              ' "\\1":\\1',
-                              fragment)
+            for pattern, replacement in substitutions.items():
+                fragment = re.sub(pattern, replacement, fragment)
 
         block += fragment
 
