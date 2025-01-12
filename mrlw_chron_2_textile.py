5c636fbcb099b8d5d99a933eca4b2cec0e879f4f
Josquin Debaz <josquindebaz@protonmail.com>
2025-01-12 17:21:23 +0100

    Extract histogram to class

--- mrlw_chron_2_textile.py
+++ mrlw_chron_2_textile.py
@@ -9,6 +9,7 @@ import re
 import libmrlwchrnck
 from CityLocator import CityLocator
 from Referencer import Referencer
+from js.Histogram import Histogram
 from sigmajs_generator import SigmaJsGenerator
 from textile_utils import format_links
 from utils import get_introduction_date, dates_to_long_dates, datetime_to_long_datetime, harmonize_domain_url
@@ -124,32 +125,9 @@ def make_histogram(block, plot_id):
     """Make a histogram in js"""
 
     core = re.split("--histo--", block)
-    series = []
-    ticks = []
-    random_color = random.choice(['#fba919', '#00749F', '#73C774', '#C7754C'])
+    histogram = Histogram(core[1], plot_id)
 
-    for fragment in re.split(";", core[1])[:-1]:
-        tick, value = re.split(",", fragment)
-        ticks.append(tick)
-        series.append(value)
-
-    series = ",".join(series)
-    ticks = "','".join(ticks)
-
-    histogram = (f"<notextile>\n "
-                 f"<script class=\"code\" type=\"text/javascript\">\n"
-                 f"$(document).ready(function()" "{"" \n"
-                 f"var s = [{series}];\n"
-                 f"var ticks = ['{ticks}'];\n"
-                 f"var plot = $.jqplot('chart_{plot_id}', [s,],""{\n"
-                 f"\tseriesColors: ['{random_color}'], \n"
-                 "\tseriesDefaults:{renderer:$.jqplot.BarRenderer, rendererOptions:{fillToZero: true}},\n\taxes:{\n"
-                 "\t\txaxis:{renderer: $.jqplot.CategoryAxisRenderer, ticks: ticks},\n"
-                 "\t\tyaxis: {pad: 1.05, tickOptions: {formatString: '%d'}}\n\t}\n});\n});\n </script>\n"
-                 "</notextile>\n\n"
-                 f"<div id=\"chart_{plot_id}\" style=\"width: 700px;\"></div>")
-
-    return core[0] + histogram + core[2]
+    return core[0] + histogram.histogram + core[2]
 
 
 def format_graphe(block, graphe_count):
