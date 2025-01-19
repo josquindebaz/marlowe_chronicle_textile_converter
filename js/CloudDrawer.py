90dbdd50b18447d9cdff8a67aec23b55ba12ec6e
Josquin Debaz <josquindebaz@protonmail.com>
2025-01-19 20:35:02 +0100

    Refactor CloudDrawer

--- js/CloudDrawer.py
+++ js/CloudDrawer.py
@@ -4,20 +4,24 @@ import re
 class CloudDrawer:
     def __init__(self, block, cloud_count):
         """format a cloud word in js"""
-        fragments = re.split("--nuage--", block)
-        block = fragments[0] + ('\n\n<notextile>\n <script '
-                                'type="text/javascript">\n '
-                                ' var word_array%d = [ ') % cloud_count
-        block += "".join([re.sub(r"\s*(.*)\s{1,},\s*(.*)\s{1,}",
-                                 '\t\t{text: "\\1", weight: \\2, color: "#"+("000000"'
-                                 '+Math.random().toString(16).slice(2, '
-                                 '8).toUpperCase()).slice(-6)},\n ',
-                                 item)
-                          for item in re.split(";", fragments[1])])
-        block += (' ];\n  $(function() { $("#cloud_%d").jQCloud(word_array%d);'
-                  '});\n </script>\n</notextile>\n\n<div id="cloud_%d" '
-                  'style="width: 700px; height: 350px;">'
-                  '</div>\n\n') % (cloud_count, cloud_count, cloud_count)
-        block += fragments[2]
 
-        self.cloud = block
+        parts = re.split("--nuage--", block)
+
+        self._cloud = parts[
+                          0] + f'\n\n<notextile>\n <script type="text/javascript">\n  var word_array{cloud_count} = [ '
+
+        for raw_value in parts[1].split(";"):
+            if raw_value:
+                key, value = re.search(r"\s*(.*)\s+,\s*(.*)\s+", raw_value).group(1, 2)
+
+                self._cloud += f'\t\t{{text: "{key}", weight: {value}, color: "#"+("000000"+Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6)}},\n '
+
+        self._cloud += (f' ];\n  $(function() {{ $("#cloud_{cloud_count}").jQCloud(word_array{cloud_count});}});\n'
+                        f' </script>\n</notextile>\n\n'
+                        f'<div id="cloud_{cloud_count}" style="width: 700px; height: 350px;"></div>\n\n')
+
+        self._cloud += parts[2]
+
+    @property
+    def cloud(self):
+        return self._cloud
